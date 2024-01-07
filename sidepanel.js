2c272259fc16c59ef8c323646505f3b90aff209c
Vivek Sekhar <vsekhar@users.noreply.github.com>
2024-01-07 18:37:58 -0500

    TODO Pull data from service worker

--- sidepanel.js
+++ sidepanel.js
@@ -4,3 +4,31 @@ import { debug } from './debug.js';
 debug("WebGPT panel loaded");
 
 document.getElementById("raw").style.visibility = settings.debug ? 'visible' : 'hidden';
+
+// TODO: remove in favor of pull approach below
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action === "updateSidePanelRaw") {
+            document.getElementById("rawData").innerHTML = request.innerText;
+            sendResponse();
+        }
+    }
+);
+
+// Listen for tab updates
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        updateSidePanel();
+    }
+});
+
+// Listen for tab activation
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    updateSidePanel();
+});
+
+function updateSidePanel() {
+    // TODO: pull side panel content from service worker so side panel can update
+    // itself when the active tab changes (currently the service worker pushes
+    // updates to the side panel via updateSidePanelRaw message).
+}
